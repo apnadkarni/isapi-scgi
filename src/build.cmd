be50f91e44952a4c55e79fd0da81bcaffa36d393
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2014-12-20 12:02:11 +0530

    Copy png files into distribution

--- src/build.cmd
+++ src/build.cmd
@@ -6,6 +6,7 @@ copy build\x86\release\isapi_scgi.dll build\isapi_scgi
 copy build\amd64\release\isapi_scgi64.dll build\isapi_scgi
 copy isapi_scgi.ini build\isapi_scgi
 copy ..\doc\isapi_scgi.html build\isapi_scgi
+copy ..\doc\*.png build\isapi_scgi
 del build\isapi_scgi.zip
 cd build && zip -r isapi_scgi.zip isapi_scgi
 cd ..
